266d121adf46c30f532073d5b9d2c87d48b5c8ba
Sevada20 <poxosyansevada@gmail.com>
2025-02-16 22:19:08 +0400

    fix buh

--- 220.am-webpage/src/Components/Navbar/Navbar.jsx
+++ 220.am-webpage/src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import './Navbar.css';
-// import logo from  '../../assets/logo_original.svg';
-
-// const Navbar = () => {
-//   return (
-//     <nav className='container'>
-//         <img src={logo} alt="" className='logo' />
-//         <ul>
-//             <li>Home</li>
-//             <li>Our Servises</li>
-//             <li>Projects</li>
-//             <li>About us</li>
-//             <li>Gallery</li>
-//             <li><button className='btn'>Contact us</button></li>
-//         </ul>
-//     </nav>
-//   )
-// }
-
-// export default Navbar
-
 import { useState, useEffect } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo_original.svg";
