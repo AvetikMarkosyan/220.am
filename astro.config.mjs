37198788944f5e3286feaa4c5575235bd0ea25a9
AvetikMarkosyan <avetik.markosyan88@gmail.com>
2025-08-13 21:34:16 +0400

    for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -17,4 +19,5 @@ export default defineConfig({
   },
 
   integrations: [react()],
+  adapter: vercel(),
 });
